16fecbc23950fdd3bc08cddb51ef1ca0d9b45f2a
SanjayRam07 <sanjayramgs07@gmail.com>
2024-03-14 22:29:46 +0530

    added-bg

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,16 +4,20 @@ import './Header.css'
 function Header() {
     return (
     <div className='header'>
-        <h1>A²I</h1>
-        <nav className='head-nav'>
-            <ul className='ul1'>
-                <li>Home</li>
-                <li>Courses</li>
-                <li>Reviews</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-        </nav>
+        <div className='head'>
+            <h1>A²I</h1>
+            <nav className='head-nav'>
+                <ul className='ul1'>
+                    <li>Home</li>
+                    <li>Courses</li>
+                    <li>Reviews</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+        <div className="body">
+        </div>
     </div>
     )
 }
